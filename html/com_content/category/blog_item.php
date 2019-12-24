7a2dd19b4c82a1b7d2b072dd9c267cc5b5a112bd
vika_naz <vikanaz98@gmail.com>
2019-12-24 13:05:50 +0400

    cleaneed blog_item

--- html/com_content/category/blog_item.php
+++ html/com_content/category/blog_item.php
@@ -1,20 +1,14 @@
 <?php defined('_JEXEC') or die; ?>
-<?php foreach($this->item->jcfields as $jcfield)
+<?php foreach($this->item->jcfields as $jcfield) //проход по массиву с доп.полями
      {
           $item->jcFields[$jcfield->name] = $jcfield;
      }
-$images = json_decode($this->item->images); 
-$link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); 
+$images = json_decode($this->item->images); //декодирование изображения
+$link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language));//получение ссылки на материал
 ?>
 
-<div class="card">
-	<a href="<?php echo $link; ?>">
-		<img src="<?php echo $images->image_intro; ?>" class="card-img-top" alt="" />
-		<div class="card-body">
-			<div><?php echo $this->item->title;?></div>
-			<?php echo $this->item->introtext;?>
-		</div>
-	</a>
+<div>
+	<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="" />
+	<a href="<?php echo $link; ?>"><?php echo $this->item->title; // название материала ?></a>
+	<?php echo $this->item->introtext; //вступительный текст материала ?>
 </div>
-
-
