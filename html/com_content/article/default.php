316a6c6ed75004cbf30da811733d84da7f039315
VikaNazarova <vikanaz98@gmail.com>
2019-12-26 21:38:03 +0400

    added structure to blog page

--- html/com_content/article/default.php
+++ html/com_content/article/default.php
@@ -1,5 +1,5 @@
 <?php defined('_JEXEC') or die; ?>
-<div class="custom item-page<?php echo $this->pageclass_sfx; ?>" itemscope itemtype="https://schema.org/Article">
+<div itemscope itemtype="https://schema.org/Article">
 	<meta itemprop="inLanguage" content="<?php echo ($this->item->language === '*') ? JFactory::getConfig()->get('language') : $this->item->language; ?>" />
 	<div itemprop="articleBody">
 		<?php echo $this->item->text; ?>
--- html/com_content/category/blog.php
+++ html/com_content/category/blog.php
@@ -1,11 +1,12 @@
 <?php defined('_JEXEC') or die; ?>
 
 <div itemscope itemtype="https://schema.org/Blog">
+	<h1><?php echo $this->category->title; ?></h1>
 	<?php foreach ($this->intro_items as $item) { 
 		$images = json_decode($item->images); //декодирование изображения
 		$link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language));//получение ссылки на материал ?>
 	<div itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting">
-		<a href="<?php echo $link; ?>"><?php echo $item->title; // название материала ?></a>
+		<h2><a href="<?php echo $link; ?>"><?php echo $item->title; // название материала ?></a></h2>
 		<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="<?php echo $item->title; ?>" />
 		<?php echo $item->introtext; //вступительный текст материала ?>
 	</div>
