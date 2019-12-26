4fafc9447da74aa09b4cca871b461153b8a3d6a9
VikaNazarova <vikanaz98@gmail.com>
2019-12-26 21:17:12 +0400

    cleaned blog + got rid of blog_item

--- html/com_content/category/blog.php
+++ html/com_content/category/blog.php
@@ -1,49 +1,13 @@
-<?php defined('_JEXEC') or die;
+<?php defined('_JEXEC') or die; ?>
 
-JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
-
-JHtml::_('behavior.caption');
-
-$dispatcher = JEventDispatcher::getInstance();
-
-$this->category->text = $this->category->description;
-$dispatcher->trigger('onContentPrepare', array($this->category->extension . '.categories', &$this->category, &$this->params, 0));
-$this->category->description = $this->category->text;
-
-$results = $dispatcher->trigger('onContentAfterTitle', array($this->category->extension . '.categories', &$this->category, &$this->params, 0));
-$afterDisplayTitle = trim(implode("\n", $results));
-
-$results = $dispatcher->trigger('onContentBeforeDisplay', array($this->category->extension . '.categories', &$this->category, &$this->params, 0));
-$beforeDisplayContent = trim(implode("\n", $results));
-
-$results = $dispatcher->trigger('onContentAfterDisplay', array($this->category->extension . '.categories', &$this->category, &$this->params, 0));
-$afterDisplayContent = trim(implode("\n", $results));
-
-?>
-<div class="blog<?php echo $this->pageclass_sfx; ?>" itemscope itemtype="https://schema.org/Blog">
-
-	<?php
-	$introcount = count($this->intro_items);
-	$counter = 0;
-	?>
-
-	<?php if (!empty($this->intro_items)) : ?>
-		<?php foreach ($this->intro_items as $key => &$item) : ?>
-			<?php $rowcount = ((int) $key % (int) $this->columns) + 1; ?>
-			<?php if ($rowcount === 1) : ?>
-				<?php $row = $counter / $this->columns; ?>
-			<?php endif; ?>
-				<div class="item" itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting">
-					<?php
-					$this->item = &$item;
-					echo $this->loadTemplate('item');
-					?>
-				</div>
-				<!-- end item -->
-				<?php $counter++; ?>
-			<?php if (($rowcount == $this->columns) or ($counter == $introcount)) : ?>
-			<?php endif; ?>
-		<?php endforeach; ?>
-	<?php endif; ?>
-
+<div itemscope itemtype="https://schema.org/Blog">
+	<?php foreach ($this->intro_items as $item) { 
+		$images = json_decode($item->images); //декодирование изображения
+		$link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language));//получение ссылки на материал ?>
+	<div itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting">
+		<a href="<?php echo $link; ?>"><?php echo $item->title; // название материала ?></a>
+		<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="<?php echo $item->title; ?>" />
+		<?php echo $item->introtext; //вступительный текст материала ?>
+	</div>
+	<?php } ?>
 </div>
