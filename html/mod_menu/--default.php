8e306139ad69ebe0b93b489f406202f98076cbce
VikaNazarova <vikanaz98@gmail.com>
2019-12-27 21:50:36 +0400

    fixed menu template

--- html/mod_menu/--default.php
+++
@@ -1,6 +0,0 @@
-<?php defined('_JEXEC') or die;
-// перебирает массив модуля
- foreach ($list as $i => &$item) {
- // выводит адрес и название пункта меню
- 	echo '<a href="/'.$item->alias.'">'.$item->title.'</a>';
- }
