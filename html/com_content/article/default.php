848a48b902ce27a140c018e1b32e16960990c83e
BloodlessRed <58694725+BloodlessRed@users.noreply.github.com>
2020-01-23 20:36:23 +0400

    Add files via upload

--- html/com_content/article/default.php
+++ html/com_content/article/default.php
@@ -2,6 +2,9 @@
 <div itemscope itemtype="https://schema.org/Article">
 	<meta itemprop="inLanguage" content="<?php echo ($this->item->language === '*') ? JFactory::getConfig()->get('language') : $this->item->language; ?>" />
 	<div itemprop="articleBody">
-		<?php echo $this->item->text; ?>
+	<?php $images = json_decode($this->item->images);?>
+	<h1><?php echo $this->item->title; ?></h1>
+	<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="" />
+	<?php echo $this->item->text; ?>
 	</div>
 </div>
