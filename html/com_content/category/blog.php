cbf70e28cf3d64105c3a088b2aee87d86e2e6b2c
BloodlessRed <58694725+BloodlessRed@users.noreply.github.com>
2020-01-25 21:38:38 +0400

    Add files via upload
    added main menu and styles for it

--- html/com_content/category/blog.php
+++ html/com_content/category/blog.php
@@ -8,34 +8,32 @@ $counter = 0;
 <div itemscope itemtype="https://schema.org/Blog"  >
 
 	<h1><?php echo $this->category->title; ?></h1>
-	<div class = "miscnews" >
-	<?php foreach ($this->intro_items as $item) { 
-		$images = json_decode($item->images); //декодирование изображения
-		$link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language));//получение ссылки на материал ?>
-	<?php if ($counter == 0) { ?>
-	<div itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting"  >
-		<?php if (!empty($images->image_intro)) { ?>
-		<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="<?php echo $item->title; ?>" />
-		<?php } ?>	
+		<div class = "miscnews" >
+			<?php foreach ($this->intro_items as $item) { 
+					$images = json_decode($item->images); //декодирование изображения
+					$link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language));//получение ссылки на материал ?>
+					<?php if ($counter == 0) { ?>
+						<div itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting"  >
+							<?php if (!empty($images->image_intro)) { ?>
+								<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="<?php echo $item->title; ?>" style = "padding-right:15px; padding-left:15px;"/>
+							<?php } ?>	
+							<div class = "inline-block">
+								<h1><a href="<?php echo $link; ?>"><?php echo $item->title; // название материала ?></a></h1>
+								<?php echo $item->introtext; //вступительный текст материала ?>
+							</div>
 		
-		<div class = "inline-block">
-		<h2><a href="<?php echo $link; ?>"><?php echo $item->title; // название материала ?></a></h2>
-		<?php echo $item->introtext; //вступительный текст материала ?>
-		</div>
-		
-	</div>
+						</div>
 	
-	<?php } else {?>
-		<div itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting"  >
-		<h2><a href="<?php echo $link; ?>"><?php echo $item->title; // название материала ?></a></h2>
-		<?php if (!empty($images->image_intro)) { ?>
-		<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="<?php echo $item->title; ?>" />
-		<?php } ?>
-		<?php echo $item->introtext; //вступительный текст материала ?>
-	</div>
-<?php } ?>
-
-	<?php $counter++; ?>
-	<?php } ?>
-	</div>
+					<?php } else {?>
+						<div itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting"  >
+							<h2><a href="<?php echo $link; ?>"><?php echo $item->title; // название материала ?></a></h2>
+							<?php if (!empty($images->image_intro)) { ?>
+								<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="<?php echo $item->title; ?>" />
+							<?php } ?>
+								<?php echo $item->introtext; //вступительный текст материала ?>
+						</div>
+					<?php } ?>
+				<?php $counter++; ?>
+			<?php } ?>
+		</div>
 </div>
