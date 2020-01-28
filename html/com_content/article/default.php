ed61b140379219f53432b0fb3b0b8c26cfc30db7
vika_naz <vikanaz98@gmail.com>
2020-01-28 14:04:02 +0400

    cleaning up after wrong merge

--- html/com_content/article/default.php
+++ html/com_content/article/default.php
@@ -1,10 +1,10 @@
-<?php defined('_JEXEC') or die; ?>
+<?php defined('_JEXEC') or die;
+$images = json_decode($this->item->images); ?>
 <div itemscope itemtype="https://schema.org/Article">
 	<meta itemprop="inLanguage" content="<?php echo ($this->item->language === '*') ? JFactory::getConfig()->get('language') : $this->item->language; ?>" />
 	<div itemprop="articleBody">
-	<?php $images = json_decode($this->item->images);?>
-	<h1><?php echo $this->item->title; ?></h1>
-	<img src="<?php echo $images->image_intro; //вступительное изображение материала ?>" alt="" />
-	<?php echo $this->item->text; ?>
+		<h1><?php echo $this->item->title; ?></h1>
+		<img src="<?php echo $images->image_intro; ?>" alt="" />
+		<?php echo $this->item->text; ?>
 	</div>
 </div>
